14b9486f676b8afa527102200f443ee610f93ec9
JuanMarinG <108723755+JuanMarinG@users.noreply.github.com>
2022-09-13 11:35:49 +0200

    Update

--- T37-Rick-Morty/src/app/characters/characters.component.ts
+++ T37-Rick-Morty/src/app/characters/characters.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersComponent implements OnInit {
 
-  constructor() { }
+  characters:any = null;
 
-  ngOnInit(): void {
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(){
+    this.http.get("https://rickandmortyapi.com/api/character/1,183,3,4,6,7,8,32")
+    .subscribe(
+      result=>{
+        this.characters = result;
+      },
+      error =>{
+        console.log('Estamos teniendo problemas al cargar los personajes');
+      }
+    );
   }
 
 }
