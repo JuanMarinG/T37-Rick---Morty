9dd2d5bef352312609f8ccf2fb615127df993cba
Juan Marin <108723755+JuanMarinG@users.noreply.github.com>
2022-09-14 12:55:35 +0200

    Revert "Service"

--- T37-Rick-Morty/src/app/characters/characters.component.ts
+++ T37-Rick-Morty/src/app/characters/characters.component.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { DatosService } from '../datos.service';
 
 @Component({
   selector: 'app-characters',
@@ -9,15 +8,20 @@ import { DatosService } from '../datos.service';
 })
 export class CharactersComponent implements OnInit {
 
+  characters:any = null;
 
-  datos:any = null;
+  constructor(private http:HttpClient) { }
 
-  constructor(private datosService: DatosService) { }
-
-  ngOnInit(): void {
-    this.datosService.retonar()
-    .subscribe(result => this.datos=result)
+  ngOnInit(){
+    this.http.get("https://rickandmortyapi.com/api/character/1,183,3,4,6,7,8,32")
+    .subscribe(
+      result=>{
+        this.characters = result;
+      },
+      error =>{
+        console.log('Estamos teniendo problemas al cargar los personajes');
+      }
+    );
   }
-  }
 
-
+}
